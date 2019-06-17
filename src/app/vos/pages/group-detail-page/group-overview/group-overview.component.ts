9c885553cfd6dbddb7fed12f1eec87e870e7d52d
Vojtěch Sassmann <vojtech.sassmann@gmail.com>
2019-06-17 14:20:26 +0200

    Subgroup detail page edit
    * On the group detail page there is now displayed link to parent group
    if the current group is a subgroup.

--- src/app/vos/pages/group-detail-page/group-overview/group-overview.component.ts
+++ src/app/vos/pages/group-detail-page/group-overview/group-overview.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {MenuItem} from '../../../../shared/models/MenuItem';
 import {ActivatedRoute} from '@angular/router';
+import {Group} from '../../../../core/models/Group';
+import {GroupService} from '../../../../core/services/api/group.service';
 
 @Component({
   selector: 'app-group-overview',
@@ -10,22 +12,37 @@ import {ActivatedRoute} from '@angular/router';
 export class GroupOverviewComponent implements OnInit {
 
   constructor(
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private groupService: GroupService
   ) { }
 
   navItems: MenuItem[] = [];
-  voId: number;
   groupId: number;
+  group: Group;
+  parentGroup: Group = null;
 
   ngOnInit() {
-    this.route.parent.params.subscribe(parentParams => {
-      this.voId = parentParams['voId'];
+    this.route.params.subscribe(params => {
+      this.groupId = params['groupId'];
 
-      this.route.params.subscribe(params => {
-        this.groupId = params['groupId'];
 
-        this.initNavItems();
+      this.groupService.getGroupById(this.groupId).subscribe(group => {
+        this.group = group;
+
+        if (this.group.parentGroupId !== null) {
+          this.loadParentGroupData();
+        } else {
+          this.parentGroup = null;
+          this.initNavItems();
+        }
       });
+    });
+  }
+
+  private loadParentGroupData() {
+    this.groupService.getGroupById(this.group.parentGroupId).subscribe(parentGroup => {
+      this.parentGroup = parentGroup;
+      this.initNavItems();
     });
   }
 
@@ -33,31 +50,31 @@ export class GroupOverviewComponent implements OnInit {
     this.navItems = [
       {
         icon: 'user-white.svg',
-        url: `/organizations/${this.voId}/groups/${this.groupId}/members`,
+        url: `/organizations/${this.group.voId}/groups/${this.groupId}/members`,
         label: 'MENU_ITEMS.GROUP.MEMBERS',
         style: 'group-btn'
       },
       {
         icon: 'group-white.svg',
-        url: `/organizations/${this.voId}/groups/${this.groupId}/subgroups`,
+        url: `/organizations/${this.group.voId}/groups/${this.groupId}/subgroups`,
         label: 'MENU_ITEMS.GROUP.SUBGROUPS',
         style: 'group-btn'
       },
       {
         icon: 'resource-white.svg',
-        url: `/organizations/${this.voId}/groups/${this.groupId}/resources`,
+        url: `/organizations/${this.group.voId}/groups/${this.groupId}/resources`,
         label: 'MENU_ITEMS.GROUP.RESOURCES',
         style: 'group-btn'
       },
       {
         icon: 'applications-white.svg',
-        url: `/organizations/${this.voId}/groups/${this.groupId}/applications`,
+        url: `/organizations/${this.group.voId}/groups/${this.groupId}/applications`,
         label: 'MENU_ITEMS.GROUP.APPLICATIONS',
         style: 'group-btn'
       },
       {
         icon: 'settings2-white.svg',
-        url: `/organizations/${this.voId}/groups/${this.groupId}/settings`,
+        url: `/organizations/${this.group.voId}/groups/${this.groupId}/settings`,
         label: 'MENU_ITEMS.GROUP.SETTINGS',
         style: 'group-btn'
       },
